41b0b7f2ce303daff904cf5ccc27872684318576
Javier Emanuel Huebra <104619744+javierhuebra@users.noreply.github.com>
2025-10-12 16:32:46 -0300

    fix

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -35,36 +35,31 @@ const Inicio = ({ lang }) => {
                         </p>
                 }
 
-                <br />
-                {
-                    lang === 'es' ?
-                        <>
-                            <p className=' text-xl text-accent text-center fontbo'>
-                                En este menú digital encontrarás todas nuestras opciones comestibles y bebidas.
-                            </p>
-                            <br />
-                            <p className=' text-xl text-accent text-center'>
-                                En caso de necesitar más información podés visitar <a href="https://kscaritas.com/" target='_blank' className='text-primary underline underline-offset-2'>nuestra web</a>.
-                            </p>
-                        </>
-                        :
-                        <>
-                            <p className=' text-xl text-accent text-center fontbo'>
-                                In this digital menu, you will find all our food and drink options.
-                            </p>
-                            <br />
-                            <p className=' text-xl text-accent text-center'>
-                                If you need more information, you can visit <a href="https://kscaritas.com/" target='_blank' className='text-primary underline underline-offset-2'>our website</a>.                            </p>
-                        </>
-                }
 
-                <div className='  flex  flex-col '>
 
-                    <Link to='/carta' className='text-center bg-primary px-4 py-4 border-[3px] border-accent rounded-lg text-2xl mt-6 text-accent shadow-lg font-bold'>Ir al Menú</Link>
 
+                <div className=' flex  flex-col '>
+
+                    <Link to='/carta' className='text-center bg-primary px-4 py-4 border-[3px] border-accent rounded-lg text-2xl mt-6 text-accent shadow-lg font-bold mb-4'>Ir al Menú</Link>
+                    {
+                        lang === 'es' ?
+                            <>
+
+                                <p className=' text-xl text-accent text-center'>
+                                    En caso de necesitar más información podés visitar <a href="https://kscaritas.com/" target='_blank' className='text-primary underline underline-offset-2'>nuestra web</a>.
+                                </p>
+                            </>
+                            :
+                            <>
+
+
+                                <p className=' text-xl text-accent text-center'>
+                                    If you need more information, you can visit <a href="https://kscaritas.com/" target='_blank' className='text-primary underline underline-offset-2'>our website</a>.                            </p>
+                            </>
+                    }
                     <div className='mb-6 flex justify-between items-center mt-6 '>
                         <Link to='https://g.page/r/CZXnwFuuEQLiEBM/review' target='_blank' className='bg-red-700 rounded-lg px-4 py-2 text-white font-bold shadow-lg flex items-center justify-start'>
-                            <p className='mr-1'>Dejar una Reseña </p><FaGooglePlusSquare className='text-2xl' />
+                            <p className='mr-1'>Calificanos en google :) </p><FaGooglePlusSquare className='text-2xl' />
                         </Link>
                         <div className='flex '>
                             <Link to='https://www.instagram.com/kscaritas/' target='_blank' className='mr-2'>
